b8ffc15228dda7fbd47ec76b822cfd0db8e6b1f3
RoastedChestnut <chestnut.roasted@outlook.com>
2026-06-13 17:00:35 +0800

    chore: update version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule OrchidStratum.MixProject do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.2.1"
   @source_url "https://github.com/SynapticStrings/OrchidStratum"
 
   def project do
